ad6b93ac43222c4b3b471f49884bb76cd817b1cf
Amine707 <ia_bacha@esi.dz>
2020-03-15 00:11:57 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Data;
+using System.Data.Common;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.Linq;
+using System.Data.OleDb;
+using System.Data.SqlClient;
+
+namespace myprojet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlConnection cnx = new SqlConnection("Data Source = .\\SQLEXPRESS; Initial Catalog = ESI_COS; Integrated Security = True");
+            cnx.Open();
+            SqlCommand cmd = cnx.CreateCommand();
+            cmd.CommandText = "SELECT * FROM employes;";
+            SqlDataReader rdr = cmd.ExecuteReader();
+            while(rdr.Read())
+            {
+                for (int i = 0; i < rdr.FieldCount; i++)
+                {
+                    Console.WriteLine(rdr.GetValue(i));
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
